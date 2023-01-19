16acdf29581f4acd13d9170ce744af7c4b97fabc
keiondesu96 <keiondesu96@gmail.com>
2023-01-19 21:50:57 +0400

    removed unnecessary imports

--- Code/Grid.cs
+++ Code/Grid.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CrossesTechTask.Code
+﻿namespace CrossesTechTask.Code
 {
     public class Grid
     {
--- Code/Program.cs
+++ Code/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading;
-
-namespace CrossesTechTask.Code
+﻿namespace CrossesTechTask.Code
 {
     internal class Program
     {
