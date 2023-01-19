3ed90dde63c73ce9e3d807fe7cf043cf257dadbf
keiondesu96 <keiondesu96@gmail.com>
2023-01-19 21:10:42 +0400

    base for winning conditions

--- Code/Grid.cs
+++ Code/Grid.cs
@@ -6,6 +6,9 @@ namespace CrossesTechTask.Code
 {
     public class Grid
     {
+        private const char CrossChar = 'X';
+        private const char CircleChar = 'O';
+
         public int FieldSize { get; private set; }
         public char[,] Field { get; private set; }
 
@@ -28,7 +31,7 @@ namespace CrossesTechTask.Code
         /// </summary>
         public bool TryPutCrossOnField(int x, int y)
         {
-            return TryPutCharOnField(x, y, 'X');
+            return TryPutCharOnField(x, y, CrossChar);
         }
 
         /// <summary>
@@ -36,7 +39,7 @@ namespace CrossesTechTask.Code
         /// </summary>
         public bool TryPutNoughtOnField(int x, int y)
         {
-            return TryPutCharOnField(x, y, 'O');
+            return TryPutCharOnField(x, y, CircleChar);
         }
 
         /// <summary>
@@ -60,6 +63,54 @@ namespace CrossesTechTask.Code
             {
                 return false;
             }
+        }
+
+        /// <summary>
+        /// Проверяет условия победы для заданного игрока: по вертикали, горизонтали и диагонали. В случае победы возвращает true, иначе false.
+        /// </summary>
+        public bool CheckWin(GameSession.TurnOf player)
+        {
+            char checkThisChar = player == GameSession.TurnOf.Player1_X ? CrossChar : CircleChar;
+            int winAmount = this.FieldSize;
+
+            bool hadWin = false;
+
+            //Вертикальные и диагональные условия победы зависят от размера: по 3-5 возможных вариантов на каждое измерение
+            for (int y = 0; y < winAmount; y++)
+                hadWin = hadWin || CheckHorizontalWinAtLine(checkThisChar, winAmount, y);
+            for (int x = 0; x < winAmount; x++)
+                hadWin = hadWin || CheckVerticalWinAtRow(checkThisChar, winAmount, x);
+
+            //Диагональных побед всегда возможно только две
+            ;
+
+            return hadWin;
+        }
+
+        private bool CheckHorizontalWinAtLine(char checkThisChar, int winAmount, int y)
+        {
+            for (int x = 0; x < winAmount; x++)
+            {
+                if (this.Field[x, y] == checkThisChar)
+                    continue;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool CheckVerticalWinAtRow(char checkThisChar, int winAmount, int x)
+        {
+            for (int y = 0; y < winAmount; y++)
+            {
+                if (this.Field[x, y] == checkThisChar)
+                    continue;
+                else
+                    return false;
+            }
+
+            return true;
         }
     }
 }
